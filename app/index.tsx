3e242dec86faaea3ebe67a5b3493160716fb9a49
abdallaahmed <95871510+abdallaahmedeltanany@users.noreply.github.com>
2025-09-24 18:27:29 +0300

    the last commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
+import AppColors from "@/constants/Colors";
 import { useAuthStore } from "@/store/authStore";
 import { useRouter } from "expo-router";
 import LottieView from "lottie-react-native";
 import React, { useRef } from "react";
-import { Dimensions, StatusBar, StyleSheet, View } from "react-native";
+import { Dimensions, StatusBar, StyleSheet, Text, View } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 const SplashScreen = () => {
@@ -20,12 +21,14 @@ const SplashScreen = () => {
   return (
     <View style={styles.container}>
       <StatusBar hidden />
+      <Text style={styles.welcomeText}>Dressify</Text>
+
       <LottieView
         ref={animationRef}
         source={require("../assets/animations/Splash.json")}
         style={styles.animation}
         autoPlay={true}
-        loop={false}
+        loop={true}
         speed={2}
         onAnimationFinish={onAnimationFinish}
         resizeMode="contain"
@@ -39,7 +42,7 @@ export default SplashScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff", // Match your brand color
+    backgroundColor: "#ffffff",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -57,8 +60,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   welcomeText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#333",
+    fontSize: 40,
+    fontFamily: "Inter-Bold",
+    color: AppColors.primary[400],
+    marginBottom: -70,
   },
 });
